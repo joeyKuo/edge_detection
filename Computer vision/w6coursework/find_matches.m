e91472ed590496145980c6b494a9830e43a6bf0c
joeyKuo <48597667+joeyKuo@users.noreply.github.com>
2023-11-04 18:31:02 +0000

    Delete Computer vision/w6coursework directory

--- Computer vision/w6coursework/find_matches.m
+++
@@ -1,9 +0,0 @@
-function pos2=find_matches(I1,pos1,I2);
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-I2=rgb2gray(I2);
-detector = detectHarrisFeatures(I2);
-pos2=detector.Location;
-pos[].length=max(pos1.length,pos2.length);
-end
-
